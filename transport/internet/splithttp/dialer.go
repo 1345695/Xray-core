3032a2c732586b5aa49d4e769ea91cbb01a97dde
脑袋比地球圆 <59483462+1345695@users.noreply.github.com>
2026-03-20 19:34:33 +0800

    起始就预热4路tcp

--- transport/internet/splithttp/dialer.go
+++ transport/internet/splithttp/dialer.go
@@ -86,6 +86,11 @@ func getHTTPClient(ctx context.Context, dest net.Destination, streamSettings *in
 		globalDialerMap[key] = xmuxManager
 	}
 
+	if mode == "stream-one" {
+		xmuxManager.EnableRoundRobin()
+		xmuxManager.WarmupConnections(streamOneForcedMaxConnections)
+	}
+
 	xmuxClient := xmuxManager.GetXmuxClient(ctx)
 	return xmuxClient.XmuxConn.(DialerClient), xmuxClient
 }
--- transport/internet/splithttp/mux.go
+++ transport/internet/splithttp/mux.go
@@ -27,6 +27,8 @@ type XmuxManager struct {
 	xmuxConfig  XmuxConfig
 	concurrency int32
 	connections int32
+	roundRobin  bool
+	roundIndex  uint64
 	newConnFunc func() XmuxConn
 	xmuxClients []*XmuxClient
 }
@@ -36,8 +38,25 @@ func NewXmuxManager(xmuxConfig XmuxConfig, newConnFunc func() XmuxConn) *XmuxMan
 		xmuxConfig:  xmuxConfig,
 		concurrency: xmuxConfig.GetNormalizedMaxConcurrency().rand(),
 		connections: xmuxConfig.GetNormalizedMaxConnections().rand(),
+		roundRobin:  false,
 		newConnFunc: newConnFunc,
 		xmuxClients: make([]*XmuxClient, 0),
+	}
+}
+
+func (m *XmuxManager) EnableRoundRobin() {
+	m.roundRobin = true
+}
+
+func (m *XmuxManager) WarmupConnections(count int32) {
+	if count <= 0 {
+		return
+	}
+	if m.connections > 0 && count > m.connections {
+		count = m.connections
+	}
+	for int32(len(m.xmuxClients)) < count {
+		m.newXmuxClient()
 	}
 }
 
@@ -85,7 +104,10 @@ func (m *XmuxManager) GetXmuxClient(ctx context.Context) *XmuxClient { // when l
 
 	if m.connections > 0 && len(m.xmuxClients) < int(m.connections) {
 		errors.LogDebug(ctx, "XMUX: creating xmuxClient because maxConnections was not hit, xmuxClients = ", len(m.xmuxClients))
-		return m.newXmuxClient()
+		if !m.roundRobin {
+			return m.newXmuxClient()
+		}
+		m.newXmuxClient()
 	}
 
 	xmuxClients := make([]*XmuxClient, 0)
@@ -102,6 +124,16 @@ func (m *XmuxManager) GetXmuxClient(ctx context.Context) *XmuxClient { // when l
 	if len(xmuxClients) == 0 {
 		errors.LogDebug(ctx, "XMUX: creating xmuxClient because maxConcurrency was hit, xmuxClients = ", len(m.xmuxClients))
 		return m.newXmuxClient()
+	}
+
+	if m.roundRobin {
+		i := m.roundIndex % uint64(len(xmuxClients))
+		m.roundIndex++
+		xmuxClient := xmuxClients[i]
+		if xmuxClient.leftUsage > 0 {
+			xmuxClient.leftUsage -= 1
+		}
+		return xmuxClient
 	}
 
 	i, _ := rand.Int(rand.Reader, big.NewInt(int64(len(xmuxClients))))
